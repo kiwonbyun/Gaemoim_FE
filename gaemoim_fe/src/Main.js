430bffdd1e2716d26a89d2469b07908c32eac800
변기원 <bkw9603@naver.com>
2022-04-09 02:12:06 +0900

    ??

--- gaemoim_fe/src/Main.js
+++ gaemoim_fe/src/Main.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
-import Badge from "./elements/badge";
 import Button from "./elements/Button";
 
 const Main = () => {
--- gaemoim_fe/src/elements/Input.js
+++ gaemoim_fe/src/elements/Input.js
@@ -2,10 +2,7 @@ import React, { Children } from "react";
 import styled from "styled-components";
 
 const Input = (props) => {
-
   const { type, label, placeholder } = props;
-
-
 
   if (type == "line") {
     return (
@@ -18,36 +15,31 @@ const Input = (props) => {
   if (type == "checkbox") {
     return (
       <React.Fragment>
-        <CheckBox {...props}/>
+        <CheckBox {...props} />
         <Label>{label}</Label>
       </React.Fragment>
     );
   }
 
   return (
-
     <React.Fragment>
       <Label>{label}</Label>
       <InputBox {...props} />
     </React.Fragment>
-
   );
-
-
-
-}
+};
 
 Input.defaultProps = {
   label: null,
   type: null,
   placeholder: null,
-}
+};
 
 const Label = styled.label`
   padding: 2px;
   font-size: 1em;
   font-weight: 600;
-`
+`;
 
 const InputBox = styled.input`
   box-sizing: border-box;
@@ -57,37 +49,35 @@ const InputBox = styled.input`
   height: 40px;
 
   border: none;
-  border-bottom: 2px solid #E6D5B8;
+  border-bottom: 2px solid #e6d5b8;
 
   placeholder: ${(props) => props.placeholder};
 
   transition: 0.3s;
-  
+
   &:focus {
     outline: none;
-    border-bottom: 2px solid #FF9B26;
+    border-bottom: 2px solid #ff9b26;
   }
-  
+
   &::placeholder {
-    font-size:0.85em;
+    font-size: 0.85em;
     color: #888;
   }
-
-`
+`;
 
 const CheckBox = styled.input`
-
-  &[type='checkbox'], &[type='checkbox']:checked {
+  &[type="checkbox"],
+  &[type="checkbox"]:checked {
     border-radius: 100%;
   }
-  &[type='checkbox'] {
+  &[type="checkbox"] {
     background-color: #fff;
-    border: 2px solid #FF9B26;
+    border: 2px solid #ff9b26;
   }
-  &[type='checkbox']:checked {
-  background-color: #FF9B26;
-}
-
-`
+  &[type="checkbox"]:checked {
+    background-color: #ff9b26;
+  }
+`;
 
 export default Input;
